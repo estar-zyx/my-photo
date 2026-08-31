3b7723cdcb0f4e32525734f2d1598e5ae33eec00
estar-zyx <1323655521@qq.com>
2026-08-31 21:09:44 +0800

    Add files via upload

--- guestbook-config.js
+++ guestbook-config.js
@@ -1,4 +1,4 @@
 window.GUESTBOOK_CONFIG = {
   workerUrl: 'https://zyx-guestbook.1323655521.workers.dev',
-  turnstileSiteKey: '0x4AAAAAAEi7ocRLLqM4lHiT'
+  turnstileSiteKey: '0x4AAAAAAAEi7ocRLLqM41HiT'
 };
